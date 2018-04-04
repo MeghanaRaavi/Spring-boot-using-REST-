ea2873819618cf8251ae76b6be8df6706d629352
Meghana <meghana4majesty@gmail.com>
2018-04-03 23:00:21 -0500

    Commit 2

---
+++ src/main/java/com/meghana/demo/Microsevice1Application.java
@@ -0,0 +1,12 @@
+package com.meghana.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Microsevice1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Microsevice1Application.class, args);
+	}
+}
